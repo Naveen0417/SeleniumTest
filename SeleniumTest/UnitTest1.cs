dda760d4f401a03ec5308670a5906f43653bdf02
Naveen GopiReddy <naveen.gopireddy@zenq.com>
2021-06-22 12:01:58 +0530

    First Commit

---
+++ SeleniumTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.in");
+            driver.Quit();
+        }
+    }
+}
